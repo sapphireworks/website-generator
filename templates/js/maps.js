ef0eb99ec6525343ddf3262fb94b73f637ae291d
kady <elkadyabdelrahman@gmail.com>
2015-03-11 23:28:29 +0200

    Reformatting code in index.html and main.css

---
+++ templates/js/maps.js
@@ -0,0 +1,9 @@
+function initialize() {
+  var mapProp = {
+    center: new google.maps.LatLng(51.508742, -0.120850),
+    zoom: 7,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
