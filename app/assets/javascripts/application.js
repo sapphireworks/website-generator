8aa4e5492923e2169fe71275f3a8e2c804d80e7e
kady <elkadyabdelrahman@gmail.com>
2015-03-23 21:31:16 +0200

    Modifying template-1's view and the layout
    Including content_for to include specific assets per template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,4 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap.min
+//= require jquery.easing.min
--- app/controllers/template_controller.rb
+++ app/controllers/template_controller.rb
@@ -5,6 +5,6 @@ class TemplateController < ApplicationController
 
 	def show
 		@template = Template.find(params[:id])
-		render :file => @template.template_path   #path should be like >> 'public/templates/template-1/index.html'
+		render :file => @template.template_path   #path should be in the scope of assets ==> 'template-1/index.html.erb'
 	end
 end
