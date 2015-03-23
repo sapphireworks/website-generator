4a593da888d29d02dcd607071afd063fa6046a7a
kady <elkadyabdelrahman@gmail.com>
2015-03-23 21:15:20 +0200

    Modifying seeds , paths should be in the scope of assets

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 template = Template.new
-template.template_path = 'app/views/template/template-1/index.html.erb'
-template.thumbnail_path = 'app/assets/images/template-1/thumb.png'
+template.template_path = 'template-1/index.html.erb'
+template.thumbnail_path = 'template-1/thumb.png'
 template.save
