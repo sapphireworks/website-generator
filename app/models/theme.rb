17304d58002247a870f429501bfc5055ab1e6dbc
mohamed-hussein-1 <mohamed.hussein.aem@gmail.com>
2015-04-16 10:39:12 +0300

    Revert "fixing theme bug inside database"

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,9 +1,4 @@
 class Theme < ActiveRecord::Base
 	belongs_to :template
-	has_many :nav_bars
-	has_many :footers  
-	has_many :about_sections
-	has_many :home_sections
-	has_many :contact_us_sections
-	has_many :catalogue_sections
+	has_many :nav_bars , :footers , :about_sections , :home_sections , :contact_us_sections , :catalogue_sections
 end
