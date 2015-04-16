3cb4289c593a9b5b3acec4d2d6c015ea773826ae
kady <elkadyabdelrahman@gmail.com>
2015-04-16 23:23:15 +0200

    Adding template-2 with components to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+#Template 1
+
 template = Template.new
 template.name = 'template-1'
 template.thumbnail_path = 'template-1/thumb.png'
-template.template_path = 'template/template-1/index.html'
+template.template_path = 'template/template-1/index.html.erb'
 template.save
 
 theme = Theme.new
@@ -56,4 +58,53 @@ footer.name = 'normal-footer'
 footer.html = "template/footers/footer_1.html.erb"
 footer.templates_id = 1
 footer.themes_id = 1
+footer.save
+
+#Template 2
+
+template = Template.new
+template.name = 'template-2'
+# template.thumbnail_path = 'template-2/thumb.png'
+template.template_path = 'template/template-2/index.html.erb'
+template.save
+
+theme = Theme.new
+theme.name = 'light-wide'
+theme.templates_id = 2
+theme.save
+
+# FIXME No nav yet
+
+home_section = HomeSection.new
+home_section.name = 'nav-eater'
+home_section.html = 'template/home-sections/home_section_2.html.erb'
+home_section.templates_id = 2
+home_section.themes_id = 2
+home_section.save
+
+about_section = AboutSection.new
+about_section.name = 'normal-about-section'
+about_section.html = 'template/about-sections/about_section_2.html.erb'
+about_section.templates_id = 2
+about_section.themes_id = 2
+about_section.save
+
+catalogue_section = CatalogueSection.new
+catalogue_section.html = 'template/catalogue-sections/catalogue_section_2.html.erb'
+catalogue_section.templates_id = 2
+catalogue_section.themes_id = 2
+catalogue_section.save
+
+contact_us_section = ContactUsSection.new
+contact_us_section.name = 'vertical-contact-with-map'
+contact_us_section.html = 'template/contact-us-sections/contact_us_section_2.html.erb'
+contact_us_section.templates_id = 2
+contact_us_section.themes_id = 2
+contact_us_section.save
+
+footer = Footer.new
+footer.name = 'normal-footer'
+footer.html = "template/footers/footer_2.html.erb"
+footer.templates_id = 2
+footer.themes_id = 2
 footer.save
