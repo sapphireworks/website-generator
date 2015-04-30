a59970af115203ef8b9b364ee5f5018f9b495106
kady <elkadyabdelrahman@gmail.com>
2015-04-30 17:44:27 +0200

    Updating the schema
    running the migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428201455) do
+ActiveRecord::Schema.define(version: 20150430153228) do
 
   create_table "about_sections", force: :cascade do |t|
     t.string   "name",         limit: 255
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150428201455) do
     t.integer  "templates_id", limit: 4
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.string   "data",         limit: 255
   end
 
   create_table "contact_us_sections", force: :cascade do |t|
