c6974600ef548372bca741f86f5394780c2779e2
Abkreno <mohamedelhamamsy789@gmail.com>
2015-05-01 18:14:08 +0300

    #122 Modifies the migration file to complete the down func

--- db/migrate/20150501145410_adjust_client_table.rb
+++ db/migrate/20150501145410_adjust_client_table.rb
@@ -8,7 +8,12 @@ class AdjustClientTable < ActiveRecord::Migration
       t.references :contact_us_sections
     end
   end
-  
+
   def down
+  	remove_column :clients, :nav_bars_id
+  	remove_column :clients, :home_sections_id
+  	remove_column :clients, :about_sections_id
+  	remove_column :clients, :catalogue_sections_id
+  	remove_column :clients, :contact_us_sections_id
   end
 end
