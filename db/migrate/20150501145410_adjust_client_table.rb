7cc7a6147fcfd6406ac5bac41cb8223e49f404f1
Abkreno <mohamedelhamamsy789@gmail.com>
2015-05-01 18:11:07 +0300

    #122 Adds references to all the sections in the db

--- db/migrate/20150501145410_adjust_client_table.rb
+++ db/migrate/20150501145410_adjust_client_table.rb
@@ -1,6 +1,14 @@
 class AdjustClientTable < ActiveRecord::Migration
   def up
+  	change_table :clients do |t|
+      t.references :nav_bars
+      t.references :home_sections
+      t.references :about_sections
+      t.references :catalogue_sections
+      t.references :contact_us_sections
+    end
   end
+  
   def down
   end
 end
