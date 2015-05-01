b906ec5085ad28a99258ede8ef8103e8cd5f001d
kady <elkadyabdelrahman@gmail.com>
2015-05-01 14:47:26 +0200

    Creating customize action in template controller

--- app/controllers/template_controller.rb
+++ app/controllers/template_controller.rb
@@ -1,4 +1,8 @@
 class TemplateController < ApplicationController
+	require 'open-uri'
+
+	base_url = "http://www.homna.com/items/"
+
 	def index
 		@templates = Template.all
 	end
@@ -16,6 +20,22 @@ class TemplateController < ApplicationController
 		render @template.template_path.to_s
 
 		# render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found
+	end
+
+	def customize
+		client = Client.find(1) #FIXME Hardcoded ! , We don't know how to authorize that client !
+
+		if client.nil?
+			flash[:error] = "you are not an authorized user , please try again"
+			redirect_to :index
+		end
+
+		if client.data.nil?
+			json = open("#{base_url}#{client.username}/fspec-lightop.json")
+			client.data = json
+			client.save
+		end
 
 	end
+
 end
