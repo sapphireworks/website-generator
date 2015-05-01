92abc988c4f7fa805359cda17cd9fe37591b515d
Abkreno <mohamedelhamamsy789@gmail.com>
2015-05-01 18:15:39 +0300

    #122 runs the migration to add changes to the schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428201455) do
+ActiveRecord::Schema.define(version: 20150501145410) do
 
   create_table "about_sections", force: :cascade do |t|
     t.string   "name",         limit: 255
@@ -31,10 +31,15 @@ ActiveRecord::Schema.define(version: 20150428201455) do
   end
 
   create_table "clients", force: :cascade do |t|
-    t.string   "username",     limit: 255
-    t.integer  "templates_id", limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "username",               limit: 255
+    t.integer  "templates_id",           limit: 4
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.integer  "nav_bars_id",            limit: 4
+    t.integer  "home_sections_id",       limit: 4
+    t.integer  "about_sections_id",      limit: 4
+    t.integer  "catalogue_sections_id",  limit: 4
+    t.integer  "contact_us_sections_id", limit: 4
   end
 
   create_table "contact_us_sections", force: :cascade do |t|
